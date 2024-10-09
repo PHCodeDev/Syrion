8d98098f53fe20e9d4c4ff6c49c10e2fdc70da76
ConnectGeniusBR <connectgeniusbr@gmail.com>
2024-10-09 14:44:51 -0300

    correção 2

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
-import { createClient } from '@supabase/supabase-js';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+import { useNavigate } from "react-router-dom";
 
 // Configuração do Supabase
-const supabaseUrl = 'https://ffqkqygvvbtotxnrlxkk.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmcWtxeWd2dmJ0b3R4bnJseGtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY1MzY4MTMsImV4cCI6MjA0MjExMjgxM30.XAB7tIbKQeamZkP0wr5ocK1aFgl2yUxDi3_PoEo6wSQ';
+const supabaseUrl = "https://ffqkqygvvbtotxnrlxkk.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmcWtxeWd2dmJ0b3R4bnJseGtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY1MzY4MTMsImV4cCI6MjA0MjExMjgxM30.XAB7tIbKQeamZkP0wr5ocK1aFgl2yUxDi3_PoEo6wSQ";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Definição do tipo para os dados do formulário
@@ -18,20 +19,22 @@ interface FormData {
 
 const LeadCaptureForm: React.FC = () => {
   const [formData, setFormData] = useState<FormData>({
-    nome: '',
-    email: '',
-    empresa: '',
-    telefone: '',
-    segmento: ''
+    nome: "",
+    email: "",
+    empresa: "",
+    telefone: "",
+    segmento: "",
   });
 
   const navigate = useNavigate(); // Hook de navegação
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value } = e.target;
-    setFormData(prevData => ({
+    setFormData((prevData) => ({
       ...prevData,
-      [name]: value
+      [name]: value,
     }));
   };
 
@@ -39,43 +42,52 @@ const LeadCaptureForm: React.FC = () => {
     e.preventDefault();
     try {
       const { data, error } = await supabase
-        .from('syrion')
+        .from("syrion")
         .insert([formData])
         .select();
 
       if (error) throw error;
 
-      console.log('Lead capturado com sucesso:', data);
+      console.log("Lead capturado com sucesso:", data);
 
       // Redirecionar para a página de agradecimento
-      navigate('/Obrigado');
+      navigate("/Obrigado");
 
       // Resetar o formulário
-      setFormData({ nome: '', email: '', empresa: '', telefone: '', segmento: '' });
+      setFormData({
+        nome: "",
+        email: "",
+        empresa: "",
+        telefone: "",
+        segmento: "",
+      });
     } catch (error) {
-      console.error('Erro ao enviar lead:', error);
-      alert('Erro ao enviar a solicitação. Por favor, tente novamente.');
+      console.error("Erro ao enviar lead:", error);
+      alert("Erro ao enviar a solicitação. Por favor, tente novamente.");
     }
   };
 
   return (
-    <div id='form' className="mx-auto border border-white rounded-xl text-[19px]">
+    <div
+      id="form"
+      className="mx-auto border border-white rounded-xl text-[19px]"
+    >
       <form
-        className="lg:w-[65vh] min-w-[45vh] lg:h-[80vh] h-full max-h-[80vh] rounded-lg flex flex-col mx-auto"
+        className="lg:w-[65vh] min-w-[40vh] mx-4 lg:h-[80vh] h-full max-h-[80vh] rounded-lg flex flex-col "
         onSubmit={handleSubmit}
       >
         <input
           placeholder="Digite seu nome:"
-          className="bg-white text-[18px] text-black min-w-[37vh] lg:w-[55vh] p-2 h-[46px] mx-auto rounded-xl mt-[5vh] mb-[2vh]"
+          className="bg-white text-[18px] text-black w-full lg:w-[55vh] p-2 h-[46px] mx-auto rounded-xl mt-[5vh] mb-[2vh]"
           name="nome"
           value={formData.nome}
           onChange={handleChange}
           required
         />
         <input
-          type="email" // Definindo o tipo como email para validação automática
+          type="email"
           placeholder="Digite seu e-mail:"
-          className="bg-white text-[18px] text-black min-w-[37vh] lg:w-[55vh] p-2 h-[46px] mx-auto rounded-xl my-[2vh]"
+          className="bg-white text-[18px] text-black w-full lg:w-[55vh] p-2 h-[46px] mx-auto rounded-xl my-[2vh]"
           name="email"
           value={formData.email}
           onChange={handleChange}
@@ -83,7 +95,7 @@ const LeadCaptureForm: React.FC = () => {
         />
         <input
           placeholder="Digite o nome da sua empresa:"
-          className="bg-white text-[18px] text-black min-w-[37vh] lg:w-[55vh] p-2 h-[46px] mx-auto rounded-xl my-[2vh]"
+          className="bg-white text-[18px] text-black w-full lg:w-[55vh] p-2 h-[46px] mx-auto rounded-xl my-[2vh]"
           name="empresa"
           value={formData.empresa}
           onChange={handleChange}
@@ -91,23 +103,29 @@ const LeadCaptureForm: React.FC = () => {
         />
         <input
           placeholder="Digite seu telefone:"
-          className="bg-white text-[18px] text-black min-w-[37vh] lg:w-[55vh] p-2 h-[46px] mx-auto rounded-xl my-[2vh]"
+          className="bg-white text-[18px] text-black w-full lg:w-[55vh] p-2 h-[46px] mx-auto rounded-xl my-[2vh]"
           name="telefone"
           value={formData.telefone}
           onChange={handleChange}
           required
         />
         <select
-          className="bg-white text-[18px] text-black min-w-[37vh] lg:w-[55vh] p-2 h-[46px] mx-auto rounded-xl my-[2vh]"
+          className="bg-white text-[18px] text-black w-full lg:w-[55vh] p-2 h-[46px] mx-auto rounded-xl my-[2vh]"
           name="segmento"
           value={formData.segmento}
           onChange={handleChange}
           required
         >
-          <option value="" disabled>Qual é o seu segmento</option>
-          <option value="Startups e Empresas de Tecnologia">Startups e Empresas de Tecnologia</option>
+          <option value="" disabled>
+            Qual é o seu segmento
+          </option>
+          <option value="Startups e Empresas de Tecnologia">
+            Startups e Empresas de Tecnologia
+          </option>
           <option value="Saúde e Bem-estar">Saúde e Bem-estar</option>
-          <option value="Educação e Cursos Online">Educação e Cursos Online</option>
+          <option value="Educação e Cursos Online">
+            Educação e Cursos Online
+          </option>
           <option value="Food Service">Food Service</option>
           <option value="E-commerce">E-commerce</option>
           <option value="Indústria">Indústria</option>
@@ -121,7 +139,7 @@ const LeadCaptureForm: React.FC = () => {
         </select>
         <button
           type="submit"
-          className="font-semibold mx-10 my-10 py-3 border border-white rounded-2xl text-center text-[18px] lg:text-[20px] hover:bg-white hover:text-orange-500 duration-500"
+          className="font-semibold mx-10 mt-2 lg:mt-8 mb-10 py-3 border border-white rounded-2xl text-center text-[18px] lg:text-[20px] hover:bg-white hover:text-orange-500 duration-500"
         >
           Aumentar minhas vendas
         </button>
